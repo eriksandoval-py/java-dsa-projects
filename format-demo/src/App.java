da0e133678c885be864ecedcc84e7093c331a381
Erik Sandoval <eriksandoval.py@gmail.com>
2024-01-20 17:36:17 -0600

    Completed format demo

--- format-demo/src/App.java
+++ format-demo/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s\n", "Degrees", "Radians", "Sine", "Cosine", "Tangent");
+
+        int degrees = 90;
+
+        double radians = Math.toRadians(degrees);
+
+        System.out.printf("%-10d%-10.4f%-10.4f%-10.4f%-10.4f\n", degrees, radians, Math.sin(radians), Math.cos(radians), Math.tan(radians));
+
+        degrees = 180;
+
+        radians = Math.toRadians(degrees);
+
+        System.out.printf("%-10d%-10.4f%-10.4f%-10.4f%-10.4f\n", degrees, radians, Math.sin(radians), Math.cos(radians), Math.tan(radians));
+        
     }
 }
